f669dacc8c08551e3108456536d05b209d9c66b6
John Guan <Guan.Ningchuan@gmail.com>
2022-02-15 16:08:00 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/zimaboard/get-started/intro">
+            to="/zimaboard/intro">
             Let's Go!
           </Link>
         </div>
